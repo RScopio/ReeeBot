6ef95bbe7d8fef34a237e76b4e7a6e214da59770
Ryan Scopio <RScopio@users.noreply.github.com>
2021-08-25 08:10:13 -0700

    Added argue_with_dad

--- response_controller.py
+++ response_controller.py
@@ -7,15 +7,14 @@ async def mention_everyone(message: discord.Message):
     print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + 'everyone was tagged, sending pic')
     images = os.listdir("""./pics""")
     imagePath = './pics/' + str(random.choice(images))
-    await message.channel.send(file=discord.File(imagePath))
+    await message.reply(file=discord.File(imagePath))
 
 async def twitch_promo(message: discord.Message):
     print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + 'twitch link posted')
-    await message.channel.send('shameless twitch promo!')
+    await message.reply('shameless twitch promo!')
 
 async def user_mentions(message: discord.Message):
     print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + message.mentions[0].name + ' was mentioned')
-
     if message.mentions[0].id == int(os.getenv('user_nerdwords')):
         if random.randint(0, 100) < 7:
             await message.channel.send('papa 🖐👁👄👁🖐')
@@ -24,15 +23,26 @@ async def user_mentions(message: discord.Message):
             await message.channel.send('mama 🖐👁👄👁🖐')
 
 async def reddit_promo(message: discord.Message):
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + ' reddit promo')
-    await message.channel.send('Thats my fave subreedit 😎')
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + 'reddit promo')
+    await message.reply('Thats my fave subreedit 😎')
+
+async def argue_with_dad(message: discord.Message):
+    if random.randint(0,100) > 50:
+        return
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + 'starting argument with dad')
+    if random.randint(0,100) > 50:
+        await message.reply(f"SHUT UP <@!{os.getenv('bot_dad')}>! I'm sick of your lame repetitive jokes!")
+    else:
+        await message.reply(content="REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE", file=discord.File('./pics/q.gif'))
 
 async def scan_message(message: discord.Message):
     if message.mention_everyone:
         await mention_everyone(message)
     elif 'twitch.tv' in message.content:
         await twitch_promo(message)
-    elif len(message.mentions) > 0:
-        await user_mentions(message)
     elif 'reddit.com/r/' in message.content:
         await reddit_promo(message)
+    elif message.author._user.id == int(os.getenv('bot_dad')):
+        await argue_with_dad(message)
+    elif len(message.mentions) > 0:
+        await user_mentions(message)
