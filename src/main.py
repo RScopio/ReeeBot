5a1fd496997ec527b1f243ff42451c57e2937f3f
Ryan Scopio <RScopio@users.noreply.github.com>
2024-03-31 08:37:27 -0700

    Refactor controllers and log file initialization

--- src/main.py
+++ src/main.py
@@ -7,19 +7,26 @@ from discord import Intents
 import atexit
 import datetime
 from dotenv import load_dotenv
-import response_controller
-import command_controller
+from controllers import response_controller, command_controller
 
 def on_exit():
     log_file.close()
 
-# load environment variables
-if os.name == 'nt':
-    TOKEN = os.getenv('DISCORD_TOKEN')
-else:
-    load_dotenv()
-    TOKEN = os.getenv('DISCORD_TOKEN')
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# initialize log file
+log_dir = os.path.join(parent_dir, 'logs')
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+log_file_path = os.path.join(log_dir, datetime.datetime.now().strftime("%Y-%m-%d") + '.txt')
+log_file = open(log_file_path, "a")
+sys.stdout = log_file
+atexit.register(on_exit)
+
+# load environment variables
+load_dotenv(os.path.join(parent_dir, '.env'), override=True)
+TOKEN = os.getenv('DISCORD_TOKEN')
+print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + 'DISCORD_TOKEN: ' + str(TOKEN))
 if TOKEN is None:
     print('DISCORD_TOKEN not found')
     exit(1)
@@ -27,13 +34,6 @@ if TOKEN is None:
 # initialize discord client
 intents = Intents.default()
 client = discord.Client(intents=intents)
-
-# initialize log file
-if not os.path.exists('logs'):
-    os.makedirs('logs')
-log_file = open('logs/' + datetime.datetime.now().strftime("%Y-%m-%d") + '.txt', "a")
-sys.stdout = log_file
-atexit.register(on_exit)
 
 # initialize controllers
 @client.event
