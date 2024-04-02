a0a453613ce5c842514e0aa4415a3900139fcc76
Ryan Scopio <RScopio@users.noreply.github.com>
2024-04-02 16:39:35 -0700

    Refactor code to use discord_component

---
+++ src/components/discord_component.py
@@ -0,0 +1,48 @@
+import os
+import datetime
+import discord
+from discord import Intents
+from dotenv import load_dotenv
+from components import language_component
+
+def start():
+# load environment variables
+    load_dotenv('.env', override=True)
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + 'DISCORD_TOKEN: ' + str(TOKEN))
+    if TOKEN is None:
+        print('DISCORD_TOKEN not found')
+        exit(1)
+
+    # initialize discord client
+    intents = Intents.default()
+    client = discord.Client(intents=intents)
+
+    # event handlers
+    @client.event
+    async def on_ready():
+        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + 'Mother give me tendies')
+
+    @client.event
+    async def on_message(message: discord.Message):
+
+        # ignore messages from the bot itself
+        if message.author == client.user:
+            return
+
+        # read the last 13 messages from the channel
+        recent_messages = [msg async for msg in message.channel.history(limit=13)]
+
+        # squash messages into a single string
+        recent_messages.reverse()
+        recent_messages = [f"{msg.author.name}: {msg.content}" for msg in recent_messages]
+        recent_messages = "\n".join(recent_messages)
+
+        # generate response
+        response = language_component.generate_response(recent_messages)
+
+        # trim response to 2000 characters
+        response = response[:2000]
+        await message.channel.send(response)
+        
+    client.run(TOKEN)
--- src/main.py
+++ src/main.py
@@ -2,47 +2,19 @@
 
 import sys
 import os
-import discord
-from discord import Intents
 import atexit
 import datetime
-from dotenv import load_dotenv
-from controllers import response_controller, command_controller
+from components import discord_component
 
 def on_exit():
     log_file.close()
 
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # initialize log file
-log_dir = os.path.join(parent_dir, 'logs')
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-log_file_path = os.path.join(log_dir, datetime.datetime.now().strftime("%Y-%m-%d") + '.txt')
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+log_file_path = os.path.join('logs', datetime.datetime.now().strftime("%Y-%m-%d") + '.txt')
 log_file = open(log_file_path, "a")
 sys.stdout = log_file
 atexit.register(on_exit)
 
-# load environment variables
-load_dotenv(os.path.join(parent_dir, '.env'), override=True)
-TOKEN = os.getenv('DISCORD_TOKEN')
-print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + 'DISCORD_TOKEN: ' + str(TOKEN))
-if TOKEN is None:
-    print('DISCORD_TOKEN not found')
-    exit(1)
-
-# initialize discord client
-intents = Intents.default()
-client = discord.Client(intents=intents)
-
-# initialize controllers
-@client.event
-async def on_ready():
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + 'Mother give me tendies')
-
-@client.event
-async def on_message(message: discord.Message):
-    await response_controller.scan_message(message)
-    await command_controller.scan_message(message)
-
-client.run(TOKEN)
+discord_component.start()
